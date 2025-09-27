a2377493330a0eb1b523ac753969b7ef1db5e384
ROHIT-ROHIT-T <rohit.du.or.21@gmail.com>
2025-09-27 14:55:29 -0600

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->list[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'ROHIT-ROHIT-T',
+    author_email = 'Rohit.du.or.21@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
